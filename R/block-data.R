6751625a2a8845becbbc9fcc8938adab74b5b0cb
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2024-12-05 20:33:41 +0100

    wip DT output

--- R/block-data.R
+++ R/block-data.R
@@ -4,9 +4,24 @@ block_server.data_block <- function(x, input = list(), ...) {
   moduleServer(
     block_uid(x, prefix = "block"),
     function(input, output, session) {
+
       fields <- fields_server(x, input)
+      result <- reactive(evaluate_block(x, values = lapply(fields, reval)))
+
+      output$result <- DT::renderDT(
+        DT::datatable(
+          result(),
+          selection = "none",
+          options = list(
+            pageLength = 5L,
+            processing = FALSE
+          )
+        ),
+        server = TRUE
+      )
+
       list(
-        result = reactive(evaluate_block(x, values = lapply(fields, reval))),
+        result = result,
         code = reactive(generate_code(x, values = lapply(fields, reval)))
       )
     }
@@ -17,6 +32,7 @@ block_server.data_block <- function(x, input = list(), ...) {
 #' @export
 block_ui.data_block <- function(x, ...) {
   tagList(
-    fields_ui(x)
+    fields_ui(x),
+    DT::dataTableOutput(block_ns(x, "result", prefix = "block"))
   )
 }
