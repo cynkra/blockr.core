48999691636de67444e3e8f54b0b32adc57ef59b
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2025-01-12 13:33:04 +0100

    add/rm connections

--- R/board-server.R
+++ R/board-server.R
@@ -91,60 +91,122 @@ board_server.board <- function(x) {
         rv <- destroy_block(input$block_select, rv)
       })
 
-      output$links <- DT::renderDT(
-        {
-          DT::datatable(
-            board_links(rv$board),
-            options = list(pageLength = 5),
-            editable = TRUE
-          )
-        },
-        server = TRUE
-      )
-
       observeEvent(input$links, {
         showModal(links_modal(session$ns))
       })
 
       link_updates <- reactiveValues(
         add = NULL,
-        rm = NULL
+        rm = NULL,
+        curr = isolate(board_links(rv$board))
+      )
+
+      output$links <- DT::renderDT(
+        {
+          DT::datatable(
+            link_updates$curr,
+            options = list(pageLength = 5),
+            editable = list(
+              target = "cell",
+              disable = list(
+                columns = which(colnames(link_updates$curr) == "id")
+              )
+            )
+          )
+        },
+        server = TRUE
       )
 
       observeEvent(input$links_cell_edit, {
 
-        orig <- board_links(rv$board)
-        temp <- orig[input$links_cell_edit$row, ]
+        row <- input$links_cell_edit$row
+        col <- input$links_cell_edit$col
 
-        temp[, input$links_cell_edit$col] <- input$links_cell_edit$value
+        rm <- link_updates$curr$id[row]
 
-        link_updates$rm <- c(
-          link_updates$rm,
-          orig$id[input$links_cell_edit$row]
+        if (!rm %in% link_updates$rm && rm %in% board_links(rv$board)$id) {
+          link_updates$rm <- c(link_updates$rm, rm)
+        }
+
+        link_updates$curr[row, col] <- input$links_cell_edit$value
+
+        new <- link_updates$curr[row, ]
+
+        if (rm %in% link_updates$add$id) {
+          link_updates$add[link_updates$add$id == rm, ] <- new
+        } else {
+          link_updates$add <- rbind(link_updates$add, new)
+        }
+      })
+
+      observeEvent(input$add_link, {
+
+        new <- data.frame(
+          id = rand_names(link_updates$curr$id),
+          from = "",
+          to = "",
+          input = ""
         )
 
-        link_updates$add <- rbind(
-          link_updates$add,
-          temp
-        )
+        link_updates$curr <- rbind(link_updates$curr, new)
+        link_updates$add <- rbind(link_updates$add, new)
+      })
+
+      observeEvent(input$rm_link, {
+
+        sel <- input$links_rows_selected
+
+        if (length(sel)) {
+
+          id <- link_updates$curr[sel, "id"]
+
+          if (id %in% board_links(rv$board)$id) {
+            link_updates$rm <- c(link_updates$rm, id)
+          } else {
+            to_rm <- which(link_updates$add$id == id)
+            link_updates$add <- link_updates$add[-to_rm, ]
+          }
+
+          link_updates$curr <- link_updates$curr[-sel, ]
+
+        } else {
+          showNotification("No row selected", type = "warning")
+        }
+      })
+
+      observeEvent(input$cancel_links, {
+        removeModal()
+        link_updates$curr <- board_links(rv$board)
       })
 
       observeEvent(input$modify_links, {
         rv <- tryCatch(
           {
-            new <- modify_links(rv$board, link_updates$add, link_updates$rm)
-            old <- board_links(rv$board)
+            if ((is.null(link_updates$add) || !nrow(link_updates$add)) &&
+                (is.null(link_updates$rm) || !length(link_updates$rm))) {
 
-            rv <- update_block_connections(
-              rv,
-              add = link_updates$add,
-              rm = old[old$id %in% link_updates$rm, ]
-            )
+              showNotification(
+                "No changes specified.",
+                type = "warning"
+              )
 
-            link_updates$add <- NULL
-            link_updates$rm <- NULL
+            } else {
 
-            rv$board <- new
+              new <- modify_links(rv$board, link_updates$add, link_updates$rm)
+              old <- board_links(rv$board)
+
+              rv <- update_block_connections(
+                rv,
+                add = link_updates$add,
+                rm = old[old$id %in% link_updates$rm, ]
+              )
+
+              link_updates$add <- NULL
+              link_updates$rm <- NULL
+              link_updates$curr <- board_links(new)
+
+              rv$board <- new
+            }
 
             removeModal()
 
@@ -165,14 +227,18 @@ board_server.board <- function(x) {
   )
 }
 
-update_block_connections <- function(rv, add, rm) {
+update_block_connections <- function(rv, add = NULL, rm = NULL) {
 
-  for (i in seq_len(nrow(rm))) {
-    rv <- do.call(destroy_connection, c(list(rv), rm[i, ]))
+  if (not_null(rm)) {
+    for (i in seq_len(nrow(rm))) {
+      rv <- do.call(destroy_connection, c(list(rv), rm[i, ]))
+    }
   }
 
-  for (i in seq_len(nrow(add))) {
-    rv <- do.call(setup_connection, c(list(rv), add[i, ]))
+  if (not_null(add)) {
+    for (i in seq_len(nrow(add))) {
+      rv <- do.call(setup_connection, c(list(rv), add[i, ]))
+    }
   }
 
   rv
@@ -183,9 +249,12 @@ links_modal <- function(ns) {
     title = "Board connections",
     DT::dataTableOutput(ns("links")),
     footer = tagList(
-      modalButton("Cancel"),
-      actionButton(ns("modify_links"), "OK")
-    )
+      actionButton(ns("add_link"), "Add row", icon = icon("plus")),
+      actionButton(ns("rm_link"), "Remove selected", icon = icon("minus")),
+      actionButton(ns("cancel_links"), "Cancel", class = "btn-danger"),
+      actionButton(ns("modify_links"), "OK", class = "btn-success")
+    ),
+    size = "l"
   )
 }
 
